62a8cb8ed90bbd7c63ac23829b1699c1de07dd8c
wailim0506 <wailim0506@icloud.com>
2024-10-10 11:36:40 +0800

    index.html

---
+++ src/js/index.js
@@ -0,0 +1,5 @@
+// $(document).ready(function(){
+//     console.log('Hello World');
+//     $('html').css('background-color', 'rgb(34,37,41)');
+//     $('p, h1, label').css('color', 'rgb(194,196,200)');
+// });
