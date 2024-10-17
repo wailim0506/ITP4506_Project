cc55896a64488ca92e91109a421d3215b79a6c13
wailim0506 <wailim0506@icloud.com>
2024-10-17 11:40:38 +0800

    carPage.html to be appended by JS

--- src/js/vehicleSales/customer/carPage.js
+++ src/js/vehicleSales/customer/carPage.js
@@ -19,12 +19,97 @@ function loadCar(){
                 //console.log(brand.car[j].id);
                 if (brand.car[j].id == id) {
                     var car = brand.car[j];
-                    // $('#carType').text(car.type);
-                    // $('#carFuel').text(car.fuelType);
-                    // $('#carPrice').text(car.price);
-                    // $('#carImage').attr('src', '../../../src/img/vehicleSales/car/' + car.image);
-                    // break;
-                    console.log(car.model);
+                    //to append:
+                    // #image1
+                    // #image2
+                    // #image3
+                    // #name
+                    // #price
+                    // #similarCarSection
+                    console.log(car.countryOfOrigin[0]);
+                    $('#name').text(car.model);
+                    // append basic infomation
+                    $('#basicInfoSection').html("");
+                    $('#basicInfoSection').append("<div class=\"infoBox\">\n" +
+                        "        <p class=\"title\">Country of Origin:</p>\n" +
+                        "        <p class=\"word\"><img\n" +
+                        `                src=\"https://flagcdn.com/${car.countryOfOrigin[0]}.svg\"\n` +
+                        "                width=\"25\"\n" +
+                        `                style=\"margin-right: 10px\">${car.countryOfOrigin[1]}</p>\n` +
+                        "      </div>");
+                    $('#basicInfoSection').append("<div class=\"infoBox\">\n" +
+                        "        <p class=\"title\">Manufacture Year:</p>\n" +
+                        `        <p class=\"word\">${car.manufactureYear}</p>\n` +
+                        "      </div>");
+                    $('#basicInfoSection').append("<div class=\"infoBox\">\n" +
+                        "        <p class=\"title\">Fuel Type:</p>\n" +
+                        `        <p class=\"word\">${car.fuelType}</p>\n` +
+                        "      </div>");
+                    $('#basicInfoSection').append("<div class=\"infoBox\">\n" +
+                        "        <p class=\"title\">Transmission:</p>\n" +
+                        `        <p class=\"word\">${car.transmission}</p>\n` +
+                        "      </div>");
+                    $('#basicInfoSection').append("<div class=\"infoBox\">\n" +
+                        "        <p class=\"title\">Engine Type:</p>\n" +
+                        `        <p class=\"word\">${car.engineType}</p>\n` +
+                        "      </div>");
+                    $('#basicInfoSection').append("<div class=\"infoBox\">\n" +
+                        "        <p class=\"title\">Horse Power:</p>\n" +
+                        `        <p class=\"word\">${car.horsepower} Watt</p>\n` +
+                        "      </div>");
+                    $('#basicInfoSection').append("<div class=\"infoBox\">\n" +
+                        "        <p class=\"title\">Max. Range:</p>\n" +
+                        `        <p class=\"word\">${car.maxRange} KM</p>\n` +
+                        "      </div>");
+                    $('#basicInfoSection').append("<div class=\"infoBox\">\n" +
+                        "        <p class=\"title\">Acceleration:</p>\n" +
+                        `        <p class=\"word\">${car.acceleration}</p>\n` +
+                        "      </div>");
+                    $('#basicInfoSection').append("<div class=\"infoBox\">\n" +
+                        "        <p class=\"title\">Drive Train:</p>\n" +
+                        `        <p class=\"word\">${car.acceleration}</p>\n` +
+                        "      </div>");
+                    $('#basicInfoSection').append("<div class=\"infoBox\">\n" +
+                        "        <p class=\"title\">Seating Capacity:</p>\n" +
+                        `        <p class=\"word\">${car.seatingCapacity}</p>\n` +
+                        "      </div>");
+
+                    // append battery information
+                    $('#batteryInfoSection').html("");
+                    $('#batteryInfoSection').append("<div class=\"infoBox batteryInfoBox\">\n" +
+                        "        <p class=\"title\">Battery Capacity:</p>\n" +
+                        `        <p class=\"word\">${car.batteryCapacity}kWh</p>\n` +
+                        "      </div>");
+                    $('#batteryInfoSection').append("<div class=\"infoBox batteryInfoBox\">\n" +
+                        "        <p class=\"title\">Charging Time:</p>\n" +
+                        "        <ul class=\"word\" id='chargingTimeList'>\n" +
+                        "        </ul>\n" +
+                        "      </div>");
+                    $.each(car.chargingTime, function (key, value) {
+                        $('#chargingTimeList').append(`<li>${key}: ${value}</li>`);
+                    });
+
+                    //append feature
+                    $('#featureInfoSection').html("");
+                    $.each(car.features, function (category, items) {
+                        const infoBox = `<div class="infoBox featureInfoBox">
+                                                    <p class="title">${category}:</p>
+                                                    <ul class="word">
+                                                        ${items.map(item => `<li>${item}</li>`).join('')}
+                                                    </ul>
+                                                </div>`;
+                        $('#featureInfoSection').append(infoBox);
+                    });
+
+                    //append warranty
+                    $('#warrantyInfoSection').html("");
+                    $.each(car.warranty, function (category, items) {
+                        const infoBox = `<div class="infoBox">
+                                                    <p class="title">${category}:</p>
+                                                    <p class="word">${items}</p>
+                                                </div>`;
+                        $('#warrantyInfoSection').append(infoBox);
+                    });
                     break;
                 }
             }
@@ -42,6 +127,78 @@ function loadCar(){
                     // $('#carImage').attr('src', '../../../src/img/vehicleSales/car/' + car.image);
                     // break;
                     console.log(car.model);
+                    $('#name').text(car.model);
+                    // append basic infomation
+                    $('#basicInfoSection').html("");
+                    $('#basicInfoSection').append("<div class=\"infoBox\">\n" +
+                        "        <p class=\"title\">Country of Origin:</p>\n" +
+                        "        <p class=\"word\"><img\n" +
+                        `                src=\"https://flagcdn.com/${car.countryOfOrigin[0]}.svg\"\n` +
+                        "                width=\"25\"\n" +
+                        `                style=\"margin-right: 10px\">${car.countryOfOrigin[1]}</p>\n` +
+                        "      </div>");
+                    $('#basicInfoSection').append("<div class=\"infoBox\">\n" +
+                        "        <p class=\"title\">Manufacture Year:</p>\n" +
+                        `        <p class=\"word\">${car.manufactureYear}</p>\n` +
+                        "      </div>");
+                    $('#basicInfoSection').append("<div class=\"infoBox\">\n" +
+                        "        <p class=\"title\">Fuel Type:</p>\n" +
+                        `        <p class=\"word\">${car.fuelType}</p>\n` +
+                        "      </div>");
+                    $('#basicInfoSection').append("<div class=\"infoBox\">\n" +
+                        "        <p class=\"title\">Transmission:</p>\n" +
+                        `        <p class=\"word\">${car.transmission}</p>\n` +
+                        "      </div>");
+                    $('#basicInfoSection').append("<div class=\"infoBox\">\n" +
+                        "        <p class=\"title\">Engine Type:</p>\n" +
+                        `        <p class=\"word\">${car.engineType}</p>\n` +
+                        "      </div>");
+                    $('#basicInfoSection').append("<div class=\"infoBox\">\n" +
+                        "        <p class=\"title\">Horse Power:</p>\n" +
+                        `        <p class=\"word\">${car.horsepower} Watt</p>\n` +
+                        "      </div>");
+                    $('#basicInfoSection').append("<div class=\"infoBox\">\n" +
+                        "        <p class=\"title\">Max. Range:</p>\n" +
+                        `        <p class=\"word\">${car.maxRange} KM</p>\n` +
+                        "      </div>");
+                    $('#basicInfoSection').append("<div class=\"infoBox\">\n" +
+                        "        <p class=\"title\">Acceleration:</p>\n" +
+                        `        <p class=\"word\">${car.acceleration}</p>\n` +
+                        "      </div>");
+                    $('#basicInfoSection').append("<div class=\"infoBox\">\n" +
+                        "        <p class=\"title\">Drive Train:</p>\n" +
+                        `        <p class=\"word\">${car.acceleration}</p>\n` +
+                        "      </div>");
+                    $('#basicInfoSection').append("<div class=\"infoBox\">\n" +
+                        "        <p class=\"title\">Seating Capacity:</p>\n" +
+                        `        <p class=\"word\">${car.seatingCapacity}</p>\n` +
+                        "      </div>");
+
+                    //remove battery section
+                    $('#batteryInfoSection').remove();
+                    $('#batterySectionTitle').remove();
+
+                    //append feature
+                    $('#featureInfoSection').html("");
+                    $.each(car.features, function (category, items) {
+                        const infoBox = `<div class="infoBox featureInfoBox">
+                                                    <p class="title">${category}:</p>
+                                                    <ul class="word">
+                                                        ${items.map(item => `<li>${item}</li>`).join('')}
+                                                    </ul>
+                                                </div>`;
+                        $('#featureInfoSection').append(infoBox);
+                    });
+
+                    //append warranty
+                    $('#warrantyInfoSection').html("");
+                    $.each(car.warranty, function (category, items) {
+                        const infoBox = `<div class="infoBox">
+                                                    <p class="title">${category}:</p>
+                                                    <p class="word">${items}</p>
+                                                </div>`;
+                        $('#warrantyInfoSection').append(infoBox);
+                    });
                     break;
                 }
             }
@@ -56,9 +213,11 @@ function setDarkMode() {
         $('html').css('background-color', 'rgb(34,37,41)');
         $('body').css('color', 'rgb(194,196,200)');
         $('a').css('color', 'rgba(255,255,255,0.65)');
-        $('.infoBox .title').css('color','white');
+        $('.infoBox .word').css('color', 'white');
         $('.section').css('border-bottom', '1px solid #4c4c4c');
-        $('.car-item').css('border', '0.1px solid grey');
+        $('#similarCarSection').css('border', '0');
+        $('.sectionTitleDiv p').css('color', 'white');
+        $('.similarCarBox').css('border', '0.1px solid grey');
         $('input').css('background-color', 'rgb(44,47,51)');
         $('input').css('border', '0.1px solid grey');
         $('input').css('color', 'rgb(194,196,200)');
@@ -72,9 +231,11 @@ function setDarkMode() {
         $('html').css('background-color', 'white');
         $('body').css('color', 'black');
         $('a').css('color', 'rgba(0,0,0,0.65)');
-        $('.infoBox .title').css('color','black');
+        $('.infoBox .word').css('color', 'black');
         $('.section').css('border-bottom', '1px solid #d1cece');
-        $('.car-item').css('border', '1px solid lightgrey');
+        $('#similarCarSection').css('border', '0');
+        $('.sectionTitleDiv p').css('color', 'black');
+        $('.similarCarBox').css('border', '1px solid lightgrey');
         $('input').css('background-color', 'white');
         $('input').css('border', '1px solid lightgrey');
         $('input').css('color', 'black');
@@ -94,7 +255,9 @@ function loadDarkMode() {
             $('a').css('color', 'rgba(255,255,255,0.65)');
             $('.infoBox .title').css('color','white');
             $('.section').css('border-bottom', '1px solid #4c4c4c');
-            $('.car-item').css('border', '0.1px solid grey');
+            $('#similarCarSection').css('border', '0');
+            $('.sectionTitleDiv p').css('color', 'white');
+            $('.similarCarBox').css('border', '0.1px solid grey');
             $('input').css('background-color', 'rgb(44,47,51)');
             $('input').css('border', '0.1px solid grey');
             $('input').css('color', 'rgb(194,196,200)');
@@ -109,7 +272,9 @@ function loadDarkMode() {
             $('a').css('color', 'rgba(0,0,0,0.65)');
             $('.infoBox .title').css('color','black');
             $('.section').css('border-bottom', '1px solid #d1cece');
-            $('.car-item').css('border', '1px solid lightgrey');
+            $('#similarCarSection').css('border', '0');
+            $('.sectionTitleDiv p').css('color', 'black');
+            $('.similarCarBox').css('border', '1px solid lightgrey');
             $('input').css('background-color', 'white');
             $('input').css('border', '1px solid lightgrey');
             $('input').css('color', 'black');
@@ -121,8 +286,11 @@ function loadDarkMode() {
     }
 }
 
+loadDarkMode();
+setDarkMode();
 $(document).ready(function () {
     loadDarkMode();
+    setDarkMode();
     loadCar();
     $('#darkModeToggle').click(function () {
         setDarkMode();
