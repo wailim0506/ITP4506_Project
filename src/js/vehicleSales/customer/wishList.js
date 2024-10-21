415fc3b08df8daa4d8a23a430845b3eb1df603c9
wailim0506 <wailim0506@icloud.com>
2024-10-21 12:38:24 +0800

    wishList.html

---
+++ src/js/vehicleSales/customer/wishList.js
@@ -0,0 +1,77 @@
+function setDarkMode() {
+    if ($('#darkModeToggle').text() == 'brightness_2') {
+        //to dark mode
+        $('html').css('background-color', 'rgb(34,37,41)');
+        $('body').css('color', 'rgb(194,196,200)');
+        $('a').css('color', 'rgba(255,255,255,0.65)');
+        $('.car-item').css('border', '0.1px solid grey');
+        $('input, select').css({
+            'background-color': 'rgb(44,47,51)',
+            'border': '0.1px solid grey',
+            'color': 'rgb(194,196,200)'
+        });
+        $('#darkModeToggle').text('wb_sunny');
+        localStorage.setItem('darkMode', 'Y');
+    } else {
+        //to light mode
+        $('html').css('background-color', 'white');
+        $('body').css('color', 'black');
+        $('a').css('color', 'rgba(0,0,0,0.65)');
+        $('.car-item').css('border', '1px solid lightgrey');
+        $('input').css({
+            'background-color': 'white',
+            'border': '1px solid lightgrey',
+            'color': 'black'
+        });
+        $('select').css({
+            'background-color': 'white',
+            'color': 'black',
+            'border': '1px solid lightgrey'
+        });
+        $('#darkModeToggle').text('brightness_2');
+        localStorage.setItem('darkMode', 'N');
+    }
+}
+
+function loadDarkMode() {
+    if (localStorage.getItem('darkMode') != null) {
+        if (localStorage.getItem('darkMode') == 'Y') {
+            $('html').css('background-color', 'rgb(34,37,41)');
+            $('body').css('color', 'rgb(194,196,200)');
+            $('a').css('color', 'rgba(255,255,255,0.65)');
+            $('.car-item').css('border', '0.1px solid grey');
+            $('input, select').css({
+                'background-color': 'rgb(44,47,51)',
+                'border': '0.1px solid grey',
+                'color': 'rgb(194,196,200)'
+            });
+            $('#darkModeToggle').text('wb_sunny');
+        } else {
+            //to light mode
+            $('html').css('background-color', 'white');
+            $('body').css('color', 'black');
+            $('a').css('color', 'rgba(0,0,0,0.65)');
+            $('.car-item').css('border', '1px solid lightgrey');
+            $('input').css({
+                'background-color': 'white',
+                'border': '1px solid lightgrey',
+                'color': 'black'
+            });
+            $('select').css({
+                'background-color': 'white',
+                'color': 'black',
+                'border': '1px solid lightgrey'
+            });
+            $('#darkModeToggle').text('brightness_2');
+        }
+    }
+}
+
+loadDarkMode();
+$(document).ready(function () {
+    loadDarkMode();
+
+    $('#darkModeToggle').click(function () {
+        setDarkMode();
+    });
+});
