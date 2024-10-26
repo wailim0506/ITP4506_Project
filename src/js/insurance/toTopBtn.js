d28dafcba8ca2df5396e6307a76432b9df7413db
hwh <230410071@stu.vtc.edu.hk>
2024-10-26 18:06:24 +0800

    icon added

--- src/js/insurance/toTopBtn.js
+++ src/js/insurance/toTopBtn.js
@@ -1,10 +1,12 @@
 /* For the to top button */
 const goTopBtn = document.querySelector('.goTopBtn');
+const chatBtn = document.querySelector('.chatBtn');
 window.addEventListener('scroll', checkHeight)
 
 function checkHeight(){
     if(window.scrollY > 200) {
         goTopBtn.style.display = "flex"
+        chatBtn.style.display = "flex"
     } else {
         goTopBtn.style.display = "none"
     }
